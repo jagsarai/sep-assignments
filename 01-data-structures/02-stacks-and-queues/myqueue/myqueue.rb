7b0872928d35f1cc6a3af7e1f13396814f786204
jagsarai <sarai.jagvir@gmail.com>
2017-11-21 17:18:18 -0800

    assignement 2 complete

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -1,18 +1,26 @@
 class MyQueue
   attr_accessor :head
   attr_accessor :tail
+  attr_accessor :count
 
   def initialize
     @queue = Array.new
-    @head = @queue[0]
+    @head = nil
+    @tail = nil
   end
 
   def enqueue(element)
+    @queue[@queue.length] = element
+    @head = @queue[0]
+    @tail = @queue[@queue.length - 1]
   end
 
   def dequeue
+    @queue.slice!(0)
   end
 
   def empty?
+    return true if @queue.length == 0
+    false
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,18 @@ class MyStack
   end
 
   def push(item)
+    @stack[@stack.length] = item 
+    @top = @stack[@stack.length - 1]
   end
 
   def pop
+    item = @stack.slice!(@stack.length - 1)
+    @top = @stack[@stack.length - 1]
+    return item
   end
 
   def empty?
+    return true if @stack.length == 0
+    false
   end
 end
