f30a175a39b9e776339afafd4ffb1a16a931bfa9
jagsarai <sarai.jagvir@gmail.com>
2017-11-30 12:30:45 -0800

    assignment complete

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -1,27 +1,70 @@
-class HashClass
+require_relative 'hash_item'
 
+class HashClass
   def initialize(size)
     @items = Array.new(size)
   end
 
   def []=(key, value)
+    item = @items[index(key, @items.size)]
+    
+    if item.class != NilClass and item.value == value
+      value
+    elsif item == nil
+      @items[index(key, @items.size)] = HashItem.new(key, value)
+    else
+      #base case of array size 1 and collision
+      if @items.size === 1
+        self.resize
+        @items[index(key, @items.size)] = HashItem.new(key, value)
+        return @items  
+      end
+
+      #array size will double until collision is resloved
+      item = @items[index(key, @items.size)]
+      until item == nil 
+        self.resize 
+        item = @items[index(key, @items.size)]
+      end
+      @items[index(key, @items.size)] = HashItem.new(key, value)
+    end
   end
 
 
   def [](key)
+    if @items[index(key, @items.size)] != nil
+      @items[index(key, @items.size)].value
+    end
   end
 
-  def resize
+  def resize 
+    temp = @items 
+    @items = Array.new(self.size * 2)
+    temp.each_with_index do |obj, key|
+      if obj != nil
+        @items[index(obj.key, @items.size)] = obj
+      else
+        @items[key] = obj
+      end
+    end 
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    index = 0
+    key.each_byte do |letter| 
+      index += letter
+    end
+    index
+    size
+    index % size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.count
   end
 
 end
